e2e381a0c3d496343abac47474275520d0330fc4
JoseLuis <joseluescu@gmail.com>
2014-11-09 17:52:48 +0100

    Constructores
    Signed-off-by: JoseLuis <joseluescu@gmail.com>

---
+++ ConstructoresObjetos/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+	//Definicion de los objeos
+		public static SegundaClase s1;
+		public static SegundaClase s2;
+		
+	public static void main(String[] args) {
+		//Creacion de los objetos
+		s1=new SegundaClase(3);
+		s2=new SegundaClase(4);
+	}
+
+}
---
+++ ConstructoresObjetos/src/SegundaClase.java
@@ -0,0 +1,14 @@
+
+public class SegundaClase {
+		//Variables
+	int num=0;
+	
+	public SegundaClase(int valor) {
+		System.out.println("El valor de num antes de guardar es el "+num);
+		
+		num=valor;
+		
+		System.out.println("El valor de num antes de guardar es el "+num);
+	}
+
+}
