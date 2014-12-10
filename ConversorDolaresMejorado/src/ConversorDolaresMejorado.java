26cafb41f4199d076aaedcc787d970b11f371384
JoseLuis <JoseLuis@telerines>
2014-12-10 23:52:19 +0100

    conversor mejorado
    Signed-off-by: JoseLuis <JoseLuis@telerines>

---
+++ ConversorDolaresMejorado/src/ConversorDolaresMejorado.java
@@ -0,0 +1,16 @@
+
+
+public class ConversorDolaresMejorado  {
+
+	//Metodo principal de nuestro conversor
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		float valorConversion=1.25f;
+	   VentanaMejorado ventana= new VentanaMejorado(valorConversion);
+	   ventana.setVisible(true);
+	}
+
+}
